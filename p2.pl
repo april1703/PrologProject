a6a402afe651e1381176b4921f5d03403da7b700
April G <127350089+april1703@users.noreply.github.com>
2025-02-11 14:35:13 -0600

    Add files via upload

---
+++ p2.pl
@@ -0,0 +1,9 @@
+/*
+Names: Sadie Sanders, April Gauthreaux
+Emails: sas111@email.latech.edu, ajg044@email.latech.edu
+Date: 02/13/2025
+Course #: CSC 330 002
+Quarter: Winter 2025
+Project #: Project 2 - Prolog
+*/
+
